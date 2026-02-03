fe6abd1a1c415101e569154e7492bc9cfdc948aa
iwaag <eiji-of-empires@nifty.com>
2026-02-04 05:24:42 +0900

    change models

--- src/agpyutils/storage.py
+++ src/agpyutils/storage.py
@@ -3,29 +3,25 @@ import httpx
 import os
 from pydantic import BaseModel
 
-class StorageResourceRef(BaseModel):
+class StoredResourceRef(BaseModel):
     key: str
     domain: str
     project: str | None = None
     is_staging: bool = False
     is_package: bool = False
 
-class ResuorceInfo(StorageResourceRef):
-    sub_resources: List[StorageResourceRef]
-
-class PresignUploadRequest(StorageResourceRef):
+class PresignUploadOption(BaseModel):
     expires_in: int | None = None
     content_type: str | None = None
 
-class PresignDownloadRequest(BaseModel):
-    storage_resource_id: str
+class PresignDownloadOption(BaseModel):
     expires_in: int | None = None
     response_content_type: str | None = None
     response_content_disposition: str | None = None
 
 class CopyObjectRequest(BaseModel):
-    source: StorageResourceRef
-    destination: StorageResourceRef
+    source_strage_resource_id: str
+    destination_strage_resource_id: str
 
 STORAGE_SERVICE_URL=os.getenv("STORAGE_SERVICE_URL")
 DOWNLOAD_PRESIGN_URL=f"{STORAGE_SERVICE_URL}/s3/presign/download"
@@ -33,21 +29,22 @@ UPLOAD_PRESIGN_URL=f"{STORAGE_SERVICE_URL}/s3/presign/upload"
 COPY_URL=f"{STORAGE_SERVICE_URL}/s3/copy"
 DELETE_URL=f"{STORAGE_SERVICE_URL}/s3/delete"
 MOVE_URL=f"{STORAGE_SERVICE_URL}/s3/move"
-async def get_download_url(auth_header: str, request: PresignDownloadRequest) -> str:
+async def get_download_url(auth_header: str, stored_resource_id: str, request: PresignDownloadOption) -> str:
     async with httpx.AsyncClient(timeout=5.0) as client:
         response = await client.post(
             DOWNLOAD_PRESIGN_URL,
             headers={"authorization": auth_header},
-            #json={"domain": domain, "key": key, "is_staging": is_staging},
+            params={"stored_resource_id": stored_resource_id},
             json=request.model_dump()
         )
         return response.json()["url"]
 
-async def get_upload_url(auth_header: str, request: PresignUploadRequest) -> str:
+async def get_upload_url(auth_header: str, stored_resource_id: str, request: PresignUploadOption) -> str:
     async with httpx.AsyncClient(timeout=5.0) as client:
         response = await client.post(
             UPLOAD_PRESIGN_URL,
             headers={"authorization": auth_header},
+            params={"stored_resource_id": stored_resource_id},
             json=request.model_dump()
         )
         return response.json()["url"]
@@ -68,7 +65,7 @@ async def copy(
         )
 async def delete(
     auth_header: str,
-    object_ref: StorageResourceRef
+    object_ref: StoredResourceRef
 ) -> str:
     #payload = {
     #    "source": {"key": source_key, "domain": source_domain, "staging": source_staging},
