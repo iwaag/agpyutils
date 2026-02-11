56ec9a03fdef888ba58251dd8a2a65577815045a
iwaag <eiji-of-empires@nifty.com>
2026-02-11 21:20:31 +0900

    change types

--- src/agpyutils/storage.py
+++ src/agpyutils/storage.py
@@ -3,24 +3,27 @@ import httpx
 import os
 from pydantic import BaseModel
 
-class BaseObjectRef(BaseModel):
+class BaseResourceRef(BaseModel):
     domain: str
     user_id: str
     project: str | None = None
 
-class StaticObjectRef(BaseObjectRef):
-    key: str
+class StaticObjectRef(BaseResourceRef):
+    relative_key: str
 
-class DynamicObjectRef(BaseObjectRef):
-    purpose: str
+class NewDynamicObjectGroupRequest(BaseResourceRef):
+    caetegory: str
+
+class DynamicObjectRef(BaseResourceRef):
     group_id: str
+    relative_key: str
 
 class PresignUploadOption(BaseModel):
-    expires_in: int | None = None
+    expires_in: int = 3600
     content_type: str | None = None
 
 class PresignDownloadOption(BaseModel):
-    expires_in: int | None = None
+    expires_in: int = 3600
     response_content_type: str | None = None
     response_content_disposition: str | None = None
 
@@ -29,68 +32,65 @@ class CopyObjectRequest(BaseModel):
     destination_strage_resource_id: str
 
 STORAGE_SERVICE_URL=os.getenv("STORAGE_SERVICE_URL")
-DOWNLOAD_PRESIGN_URL=f"{STORAGE_SERVICE_URL}/s3/presign/download"
-UPLOAD_PRESIGN_URL=f"{STORAGE_SERVICE_URL}/s3/presign/upload"
-COPY_URL=f"{STORAGE_SERVICE_URL}/s3/copy"
-DELETE_URL=f"{STORAGE_SERVICE_URL}/s3/delete"
-MOVE_URL=f"{STORAGE_SERVICE_URL}/s3/move"
+STATIC_DOWNLOAD_PRESIGN_URL=f"{STORAGE_SERVICE_URL}/static_object/download"
+STATIC_UPLOAD_PRESIGN_URL=f"{STORAGE_SERVICE_URL}/static_object/upload"
+DYNAMIC_DOWNLOAD_PRESIGN_URL=f"{STORAGE_SERVICE_URL}/dynamic_object/download"
+DYNAMIC_UPLOAD_PRESIGN_URL=f"{STORAGE_SERVICE_URL}/dynamic_object/upload"
 
-async def get_download_url(
+async def get_static_object_download_url(
     auth_header: str,
-    stored_resource_id: str,
-    request: PresignDownloadOption
+    object_ref: StaticObjectRef,
+    option: PresignDownloadOption = PresignDownloadOption()
 ) -> str:
     async with httpx.AsyncClient(timeout=5.0) as client:
         response = await client.post(
-            DOWNLOAD_PRESIGN_URL,
+            STATIC_DOWNLOAD_PRESIGN_URL,
             headers={"authorization": auth_header},
-            params={"stored_resource_id": stored_resource_id},
-            json=request.model_dump()
+            json={"ref": object_ref, "option": option.model_dump()}
         )
         return response.json()["url"]
 
-async def get_upload_url(
+async def get_static_object_upload_url(
     auth_header: str,
-    stored_resource_id: str, 
-    request: PresignUploadOption
+    object_ref: StaticObjectRef, 
+    option: PresignUploadOption = PresignUploadOption()
 ) -> str:
     async with httpx.AsyncClient(timeout=5.0) as client:
         response = await client.post(
-            UPLOAD_PRESIGN_URL,
+            STATIC_UPLOAD_PRESIGN_URL,
             headers={"authorization": auth_header},
-            params={"stored_resource_id": stored_resource_id},
-            json=request.model_dump()
+            json={"ref": object_ref, "option": option.model_dump()}
         )
         return response.json()["url"]
 
 
-async def copy(
-    auth_header: str,
-    request: CopyObjectRequest
-) -> str:
-    async with httpx.AsyncClient(timeout=5.0) as client:
-        response = await client.post(
-            COPY_URL,
-            headers={"authorization": auth_header},
-            json=request.model_dump()
-        )
-async def delete(
-    auth_header: str,
-    object_ref: DynamicObjectRef
-) -> str:
-    async with httpx.AsyncClient(timeout=5.0) as client:
-        response = await client.post(
-            DELETE_URL,
-            headers={"authorization": auth_header},
-            json=object_ref.model_dump()
-        )
-async def move(
-    auth_header: str,
-    request: CopyObjectRequest
-) -> str:
-    async with httpx.AsyncClient(timeout=5.0) as client:
-        response = await client.post(
-            MOVE_URL,
-            headers={"authorization": auth_header},
-            json=request.model_dump()
-        )
+# async def copy(
+#     auth_header: str,
+#     request: CopyObjectRequest
+# ) -> str:
+#     async with httpx.AsyncClient(timeout=5.0) as client:
+#         response = await client.post(
+#             COPY_URL,
+#             headers={"authorization": auth_header},
+#             json=request.model_dump()
+#         )
+# async def delete(
+#     auth_header: str,
+#     object_ref: DynamicObjectRef
+# ) -> str:
+#     async with httpx.AsyncClient(timeout=5.0) as client:
+#         response = await client.post(
+#             DELETE_URL,
+#             headers={"authorization": auth_header},
+#             json=object_ref.model_dump()
+#         )
+# async def move(
+#     auth_header: str,
+#     request: CopyObjectRequest
+# ) -> str:
+#     async with httpx.AsyncClient(timeout=5.0) as client:
+#         response = await client.post(
+#             MOVE_URL,
+#             headers={"authorization": auth_header},
+#             json=request.model_dump()
+#         )
